de54103e06033b1f45ee28bdb567f8fe45495976
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-06 10:25:43 +0200

    install cookies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./component/Home"
-import Confirmation from "./component/Confirmation"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import Confirmation from "./component/Confirmation";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/confirmation" element={<Confirmation/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

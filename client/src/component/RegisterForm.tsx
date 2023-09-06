b176e077f1b765ae1ec274d4b57333d66e678ad5
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-06 10:23:03 +0200

    register form for user, frontend

---
+++ client/src/component/RegisterForm.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+function RegisterForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Registreringsdata:", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Registrera dig</h2>
+
+      <label>
+        Användernamn:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+
+      <br />
+      <label>
+        E-postadress:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+      </label>
+
+      <br />
+      <label>
+        Lösenord:
+        <input
+          type="text"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <button type="submit">Registrera</button>
+      </label>
+    </form>
+  );
+}
+
+export default RegisterForm;

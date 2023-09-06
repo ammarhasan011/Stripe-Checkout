e315939e46854bb968bc9d7aab14fbc64e9b4c32
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-06 10:23:22 +0200

    cleaner home

--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -1,33 +1,12 @@
+import RegisterForm from "./RegisterForm";
+import Checkout from "./Checkout";
+
 function Home() {
-  async function handlePayment() {
-    try {
-      const response = await fetch(
-        "http://localhost:3000/stripe/create-checkout-session",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-
-          body: JSON.stringify({}),
-        }
-      );
-
-      if (!response.ok) {
-        console.error("Begäran misslyckades");
-        return;
-      }
-
-      const { url } = await response.json();
-      window.location = url;
-    } catch (error) {
-      console.error("Ett fel inträffade:", error);
-    }
-  }
-
   return (
     <div>
-      <button onClick={handlePayment}>ge mig pengar</button>
+      <Checkout />
+      <br />
+      <RegisterForm />
     </div>
   );
 }

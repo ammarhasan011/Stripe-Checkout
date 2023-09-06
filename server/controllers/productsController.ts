c1115a938f1b683d2b673ca9a598433e55bbb583
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-07 00:54:44 +0200

    show array with products

--- server/controllers/productsController.ts
+++ server/controllers/productsController.ts
@@ -4,8 +4,26 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 async function getAllProducts(req: Request, res: Response) {
   try {
     const products = await stripe.products.list();
+    const productsWithInfo: any[] = [];
 
-    res.json(products.data);
+    for (const product of products.data) {
+      const prices = await stripe.prices.list({ product: product.id });
+
+      if (prices.data.length > 0) {
+        const price = prices.data[0];
+
+        const productInfo = {
+          id: product.id,
+          name: product.name,
+          description: product.description,
+          image: product.images[0],
+          price: price.unit_amount / 100,
+        };
+        productsWithInfo.push(productInfo);
+      }
+    }
+
+    res.json(productsWithInfo);
   } catch (error) {
     console.error("Error fetching products from Stripe:", error);
     res.status(500).json({ error: "Unable to fetch products" });

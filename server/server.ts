52389be30652191701d4b53db7e6d4d7a54a8c13
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-06 13:40:53 +0200

    app.use for user

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const stripeRouter = require("./routes/stripeRouter");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const customerRoute = require("./routes/customerRoute");
 
 const app = express();
 
@@ -10,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 //Routes
-app.use("/stripe", stripeRouter);
-// app.use("/user", userRouter);
+app.use("/checkout", stripeRouter);
+app.use("/user", customerRoute);
 
 app.listen(3000, () => console.log("Server is up and runninnn..."));

ffc05ca2d66bf2d72a114dbf81a480ecde43f988
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-07 21:47:50 +0200

    cart to home

--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -1,18 +1,22 @@
 import RegisterForm from "./registerform/RegisterForm";
 import Checkout from "./checkout/Checkout";
 import ProductList from "./products/products";
-import LoginForm from "./login/loginForm";
+import LoginForm from "./login/LoginForm";
+import Cart from "./cart/Cart";
 
 function Home() {
   return (
     <div>
       <Checkout />
       <br />
+
       <RegisterForm />
       <br />
       <LoginForm />
       <br />
       <ProductList />
+      <br />
+      <Cart />
     </div>
   );
 }

a2dec420dddda656a925f851207c8d6125159e94
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-08 13:26:54 +0200

    remove unnecessary stuff

--- server/typings/express.d.ts
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-
-// declare global {
-//   namespace Express {
-//     interface Request {
-//       session?: Record<string, any>; // Lägg till detta för att inkludera sessionsegenskapen
-//     }
-//   }
-// }

b72a105d152e91e521f34ea5cc4d30d3ffb5a36a
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-09 15:50:02 +0200

    removed some component

--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -1,25 +1,25 @@
-import RegisterForm from "./registerform/RegisterForm";
-import Checkout from "./checkout/Checkout";
+// import RegisterForm from "./registerform/RegisterForm";
+// import Checkout from "./checkout/Checkout";
 import ProductList from "./products/products";
 // import LoginForm from "./login/LoginForm";
 import Cart from "./cart/Cart";
-import Header from "./Header/Header";
+// import Header from "./Header/Header";
 
 function Home() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <br />
-      <Checkout />
+      {/* <Checkout /> */}
       <br />
 
-      <RegisterForm />
+      {/* <RegisterForm /> */}
       <br />
       {/* <LoginForm /> */}
       <br />
       <ProductList />
       <br />
-      <Cart />
+      {/* <Cart /> */}
     </div>
   );
 }

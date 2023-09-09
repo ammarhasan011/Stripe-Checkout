3780b27e76f55f9d69dc0d2ec1cd4848baed0120
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-09 15:49:44 +0200

    added komponents from home to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 import Confirmation from "./component/Confirmation";
+import Header from "./component/Header/Header";
+import LoginForm from "./component/login/LoginForm";
+import RegisterForm from "./component/registerform/RegisterForm";
+import Cart from "./component/cart/Cart";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user/loginUser" element={<LoginForm />} />
+        <Route path="/user/registerUser" element={<RegisterForm />} />
+        <Route path="/cart" element={<Cart />} />
+
         <Route path="/confirmation" element={<Confirmation />} />
       </Routes>
     </BrowserRouter>

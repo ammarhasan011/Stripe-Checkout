1f3948bb33d8f328f73986039911df24d9d3e869
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-13 11:03:52 +0200

    promo code

--- client/src/component/Confirmation.tsx
+++ client/src/component/Confirmation.tsx
@@ -1,9 +1,33 @@
-
-
 function Confirmation() {
-  return (
-    <div>tack för ditt köp</div>
-  )
+  return <div>tack för ditt köp</div>;
 }
 
-export default Confirmation
+export default Confirmation;
+
+// import { useEffect, useState } from "react";
+
+// function Confirmation() {
+//   const [isPaymentVerified, setIsPaymentVerified] = useState(false);
+
+//   useEffect(() => {
+//     const session;
+
+//     const verifyPayment = async () => {
+//       const response = await fetch("http://localhost:3000/verify-session", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(cart),
+//       });
+//     };
+//   });
+
+//   return isPaymentVerified ? (
+//     <h1>Tack för ditt köp</h1>
+//   ) : (
+//     <h2>Något gick fel med betalningen</h2>
+//   );
+// }
+
+// export default Confirmation;

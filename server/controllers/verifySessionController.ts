3fbe328d985533103cacc75bc07713cb491c81f0
Ammar Hasan <ammarhasan011@gmail.com>
2023-09-15 15:29:20 +0200

    oder

--- server/controllers/verifySessionController.ts
+++ server/controllers/verifySessionController.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from "express";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-function checkVerifySession(req: Request, res: Response) {
+async function checkVerifySession(req: Request, res: Response) {
   try {
-    console.log("sessionId", req.body.sessionId);
+    // console.log("sessionId", req.body.sessionId);
+    //retrive session from stripe
+    const session = await stripe.checkout.sessions.retrieve(req.body.sessionId);
+
+    if (session.payment_status !== "paid") {
+      return res.status(400).json({ verified: false });
+    }
+
+    const order = {
+      created: session.created,
+      customer: session.customer_details.name,
+    };
+
+    console.log("Order", order);
+
     res.status(200).json({ verified: true });
   } catch (error) {
     console.log(error);
